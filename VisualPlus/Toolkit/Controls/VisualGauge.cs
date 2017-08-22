707b8c22f0eb1b30c079ce47fc1a6293ad31083b
DarkByte7 <DarkByte7@Live.com>
2017-08-22 09:34:19 -0500

    Added future controls

---
+++ VisualPlus/Toolkit/Controls/VisualGauge.cs
@@ -0,0 +1,31 @@
+﻿namespace VisualPlus.Toolkit.Controls
+{
+    #region Namespace
+
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    using VisualPlus.Toolkit.VisualBase;
+
+    #endregion
+
+    [ToolboxItem(false)]
+    [ToolboxBitmap(typeof(ProgressBar))]
+    [DefaultEvent("Click")]
+    [DefaultProperty("Value")]
+    [Description("The Visual Gauge")]
+
+    // [Designer(ControlManager.FilterProperties.VisualProgressBar)]
+    public class VisualGauge : VisualControlBase
+    {
+        #region Constructors
+
+        public VisualGauge()
+        {
+            BackColor = Color.Transparent;
+        }
+
+        #endregion
+    }
+}

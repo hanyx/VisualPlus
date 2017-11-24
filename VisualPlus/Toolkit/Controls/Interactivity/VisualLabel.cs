69c10d72b8497b62b8145ca299806a7ae828bcb3
DarkByte7 <DarkByte7@Live.com>
2017-11-24 14:35:58 -0600

    Enabled multi string editor for VisualLabel

--- VisualPlus/Toolkit/Controls/Interactivity/VisualLabel.cs
+++ VisualPlus/Toolkit/Controls/Interactivity/VisualLabel.cs
@@ -5,6 +5,7 @@
     using System;
     using System.ComponentModel;
     using System.Drawing;
+    using System.Drawing.Design;
     using System.Drawing.Drawing2D;
     using System.Drawing.Text;
     using System.Runtime.InteropServices;
@@ -280,7 +281,7 @@
             }
         }
 
-        // [Editor("System.ComponentModel.Design.MultilineStringEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
+        [Editor("System.ComponentModel.Design.MultilineStringEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
         public override string Text
         {
             get

56b61ac2113fb000029bbd31d2cddf7753c955b5
DarkByte7 <DarkByte7@Live.com>
2018-01-01 13:46:32 -0600

    Added: Image extension methods

---
+++ VisualPlus/Extensibility/ImageExtensions.cs
@@ -0,0 +1,62 @@
+﻿namespace VisualPlus.Extensibility
+{
+    #region Namespace
+
+    using System;
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.IO;
+
+    #endregion
+
+    public static class ImageExtensions
+    {
+        #region Events
+
+        /// <summary>Creates a Base64 string value from the image.</summary>
+        /// <param name="image">The image.</param>
+        /// <returns>The <see cref="string" />.</returns>
+        public static string ToBase64(this Image image)
+        {
+            using (MemoryStream _base64 = new MemoryStream())
+            {
+                image.Save(_base64, image.RawFormat);
+                image.Dispose();
+                return Convert.ToBase64String(_base64.ToArray());
+            }
+        }
+
+        /// <summary>Creates a pen from the image.</summary>
+        /// <param name="image">The image.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="startCap">The start cap.</param>
+        /// <param name="endCap">The end cap.</param>
+        /// <returns>The <see cref="Pen" />.</returns>
+        public static Pen ToPen(this Image image, float width = 1, LineCap startCap = LineCap.Custom, LineCap endCap = LineCap.Custom)
+        {
+            using (TextureBrush _textureBrush = new TextureBrush(image))
+            {
+                return new Pen(_textureBrush, width) { StartCap = startCap, EndCap = endCap };
+            }
+        }
+
+        /// <summary>Creates a texture brush from the image.</summary>
+        /// <param name="image">The image.</param>
+        /// <returns>The <see cref="TextureBrush" />.</returns>
+        public static TextureBrush ToTextureBrush(this Image image)
+        {
+            return new TextureBrush(image);
+        }
+
+        /// <summary>Creates a texture brush from the image.</summary>
+        /// <param name="image">The image.</param>
+        /// <param name="rectangle">The rectangle boundaries.</param>
+        /// <returns>The <see cref="TextureBrush" />.</returns>
+        public static TextureBrush ToTextureBrush(this Image image, Rectangle rectangle)
+        {
+            return new TextureBrush(image, rectangle);
+        }
+
+        #endregion
+    }
+}
--- VisualPlus/Managers/ImageManager.cs
+++ VisualPlus/Managers/ImageManager.cs
@@ -7,6 +7,7 @@
     using System.Drawing;
     using System.Drawing.Imaging;
     using System.Globalization;
+    using System.IO;
     using System.Windows.Forms;
 
     #endregion
@@ -38,6 +39,63 @@
             }
 
             return _bitmap;
+        }
+
+        /// <summary>Create the image from a Base64 value.</summary>
+        /// <param name="value">The Base64 value.</param>
+        /// <returns>The <see cref="Image" />.</returns>
+        public static Image DrawImageFromBase64(string value)
+        {
+            Image _image;
+            using (MemoryStream _memoryStream = new MemoryStream(Convert.FromBase64String(value)))
+            {
+                _image = Image.FromStream(_memoryStream);
+            }
+
+            return _image;
+        }
+
+        /// <summary>Draws the image with a custom color overlay.</summary>
+        /// <param name="graphics">The graphics to draw on.</param>
+        /// <param name="rectangle">The rectangle.</param>
+        /// <param name="image">The image.</param>
+        /// <param name="color">The color.</param>
+        public static void DrawImageWithColorOverlay(Graphics graphics, Rectangle rectangle, Image image, Color color)
+        {
+            float[][] _colorMatrix =
+                {
+                    new[] { Convert.ToSingle(color.R / 255.0), 0f, 0f, 0f, 0f },
+                    new[] { 0f, Convert.ToSingle(color.G / 255.0), 0f, 0f, 0f },
+                    new[] { 0f, 0f, Convert.ToSingle(color.B / 255.0), 0f, 0f },
+                    new[] { 0f, 0f, 0f, Convert.ToSingle(color.A / 255.0), 0f },
+                    new[]
+                        {
+                            Convert.ToSingle(color.R / 255.0),
+                            Convert.ToSingle(color.G / 255.0),
+                            Convert.ToSingle(color.B / 255.0), 0f,
+                            Convert.ToSingle(color.A / 255.0)
+                        }
+                };
+
+            ImageAttributes _imageAttributes = new ImageAttributes();
+            _imageAttributes.SetColorMatrix(new ColorMatrix(_colorMatrix), ColorMatrixFlag.Default, ColorAdjustType.Default);
+            graphics.DrawImage(image, rectangle, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, _imageAttributes);
+            image.Dispose();
+        }
+
+        /// <summary>Draws the image with a custom transparency.</summary>
+        /// <param name="graphics">The graphics to draw on.</param>
+        /// <param name="alpha">The alpha.</param>
+        /// <param name="rectangle">The rectangle.</param>
+        /// <param name="image">The image.</param>
+        /// <param name="color">The color.</param>
+        public static void DrawTransparentImage(Graphics graphics, float alpha, Rectangle rectangle, Image image, Color color)
+        {
+            ColorMatrix _colorMatrix = new ColorMatrix { Matrix33 = alpha };
+            ImageAttributes _imageAttributes = new ImageAttributes();
+            _imageAttributes.SetColorMatrix(_colorMatrix);
+            graphics.DrawImage(image, new Rectangle(rectangle.X, rectangle.Y, image.Width, image.Height), rectangle.X, rectangle.Y, image.Width, image.Height, GraphicsUnit.Pixel, _imageAttributes);
+            _imageAttributes.Dispose();
         }
 
         /// <summary>Sets the image opacity.</summary>

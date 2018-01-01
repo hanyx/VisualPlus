7f80cb49944d511ebae68174e74a438647ce7428
DarkByte7 <DarkByte7@Live.com>
2018-01-01 13:26:19 -0600

    Added: TextManager

---
+++ VisualPlus/Managers/TextManager.cs
@@ -0,0 +1,24 @@
+﻿namespace VisualPlus.Managers
+{
+    #region Namespace
+
+    using System.Drawing;
+
+    #endregion
+
+    public sealed class TextManager
+    {
+        #region Events
+
+        /// <summary>Set the string format alignments.</summary>
+        /// <param name="horizontalAlignment">The horizontal alignment.</param>
+        /// <param name="verticalAlignment">The vertical alignment.</param>
+        /// <returns>The <see cref="StringFormat" />.</returns>
+        public static StringFormat SetStringFormat(StringAlignment horizontalAlignment = StringAlignment.Center, StringAlignment verticalAlignment = StringAlignment.Center)
+        {
+            return new StringFormat { Alignment = horizontalAlignment, LineAlignment = verticalAlignment };
+        }
+
+        #endregion
+    }
+}
